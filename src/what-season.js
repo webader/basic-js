14619f67d85eb04ce55f7d696b01da1417fc2291
webader <alexey.derenko@gmail.com>
2022-10-17 14:40:13 +0300

    feat: what-season complete

--- src/what-season.js
+++ src/what-season.js
@@ -1,21 +1,68 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Extract season from given date and expose the enemy scout!
- * 
+ *
  * @param {Date | FakeDate} date real or fake date
  * @returns {String} time of the year
- * 
+ *
  * @example
- * 
+ *
  * getSeason(new Date(2020, 02, 31)) => 'spring'
- * 
+ *
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+const seasons = [
+  {
+    nameSeason: "spring",
+    monthStart: 03,
+    dayStart: 01,
+    monthEnd: 05,
+    dayEnd: 31,
+  },
+  {
+    nameSeason: "summer",
+    monthStart: 06,
+    dayStart: 01,
+    monthEnd: 08,
+    dayEnd: 31,
+  },
+  {
+    nameSeason: "autumn",
+    monthStart: 09,
+    dayStart: 01,
+    monthEnd: 11,
+    dayEnd: 30,
+  },
+  {
+    nameSeason: "spring",
+    monthStart: 12,
+    dayStart: 01,
+    monthEnd: 02,
+    dayEnd: 28,
+  },
+];
+function getSeason(date) {
+  try {
+    if (!date) return "Unable to determine the time of year!";
+
+    if (!(date instanceof Date)) throw new Error("Invalid date!");
+
+    if (new Date().toString() === date.toString())
+      throw new Error("Invalid date!");
+
+    const monthIndex = new Date(date).getMonth() + 1;
+
+    if ((monthIndex >= 0 && monthIndex <= 2) || monthIndex === 12)
+      return "winter";
+    if (monthIndex >= 3 && monthIndex <= 5) return "spring";
+    if (monthIndex >= 6 && monthIndex <= 8) return "summer";
+
+    if (monthIndex >= 9 && monthIndex <= 11) return "autumn";
+  } catch (error) {
+    throw Error(error.message);
+  }
 }
 
 module.exports = {
-  getSeason
+  getSeason,
 };
